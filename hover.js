c5d77f5fc092e8ea1513b51d17c438ed892d55d0
5ennette <43222445+5ennette@users.noreply.github.com>
2024-11-25 18:12:37 +0700

    1

--- hover.js
+++ hover.js
@@ -1,23 +1,31 @@
 var clickcheck=0;
 var heroside="hero-left";
 
+$(document).ready(function() {
+	$("body").css("display", "none");
+	$("body").fadeIn(1000);
+	$("#block").fadeout(500);
+});
+
 window.onclick =function(){
 	clickcheck ++;
 	if(clickcheck==1){
 		heroside = "hero-right";
-	} else if(clickcheck>1){
+	} else if(clickcheck==2){
 		heroside = "";
 		var griddisplay = document.getElementById("heroes");
 		griddisplay.style.opacity=0;
 		griddisplay = document.getElementById("versus");
 		griddisplay.style.opacity=1;
 		griddisplay.style.top="	300px";
+	} else{
+	window.location.reload(true);
 	}
 }
 
 $('.heroes_img').mouseover(function(eventObj) 
 {
 	var hoverid = eventObj.target.id;
-	var outurl= "url(images/"+hoverid+"/hero.png)";
+	var outurl= "url(images/"+hoverid+"/hero.webp)";
 	document.getElementById(heroside).style.backgroundImage = outurl;	
 })
